17ddc9b28bcaf8e0acb02d9d9c0fd0e322235a81
Vibesdk <vibesdk-bot@cloudflare.com>
2026-02-27 15:35:38 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add react-syntax-highlighter@^15.5.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
